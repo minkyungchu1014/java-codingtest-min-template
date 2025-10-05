e1ee4b4c3ef24d711070d75ed3dd29e58b1c3603
minkyungchu1014 <mkchu125@gmail.com>
2025-10-06 02:19:45 +0900

    2025-10-06 [8주차] 백트래킹_양궁대회

--- Week8Solution2.java
+++ Week8Solution2.java
@@ -6,20 +6,39 @@ class Week8Solution2 {
     private static int[] apeach;
 
     //dfs로 모든 경우 탐색
-    private static void dfs(int n, int count, int[] ryan) {
-        //n이 0이되면 점수차이 계산
+    private static void dfs(int n, int idx, int[] ryan) {
+        //n이 0이되면 점수차이 계산(화살을 다 썼을 경우)
         if (n == 0) {
             calculate(ryan);
 //            System.out.print(Arrays.toString(ryan) + " : " + getScore(ryan) + "\n");
             return;
         }
-        for (int i = 0; i <= 10; i++) {
+
+        //마지막 10점 과녁에 다다르면 남은 화살을 모두 꽂음
+        //평가 누락 방지
+        if (idx == 10) {
+            ryan[10] += n;
+            calculate(ryan);
+            ryan[10] -= n;
+            return;
+        }
+
+        for (int i = idx; i <= 10; i++) {
             if (n >= apeach[i]+1) {
                 ryan[i] = apeach[i] + 1;
                 dfs(n - ryan[i], i+1, ryan);
                 ryan[i] = 0;
             }
         }
+
+//        저자님 풀이!
+//        for (int i = 0; i <= 10; i++) {
+//            int cnt = Math.min(n, apeach[i]+1);
+//            ryan[i] = cnt;
+//            dfs(n - cnt, i+1, ryan);
+//            ryan[i] = 0;
+//            }
+//        }
     }
 
     //apeach와 ryan의 점수 차이 계산
